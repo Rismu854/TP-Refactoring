7e1b6412a6b42bed748b30d12387b6998fe4a080
Rismu854 <rravoavy@gmail.com>
2025-04-08 16:08:43 +0200

    Création de la methode main

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -4,4 +4,15 @@ public class Femme extends Humain {
 	public Femme(String nom,String prenom,int age) {
 		super(nom ,prenom, age);
 	}
+	
+	public static void main(String[] args) {
+		Homme jean = new Homme("Dupont", "Jean", 28);
+		Femme marie = new Femme("Martin", "Marie", 28);
+		
+		jean.ami(marie);
+		marie.ami(jean);
+		
+		jean.details();
+		marie.details();
+	}
 }
