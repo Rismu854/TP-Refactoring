58f940d9543bb03ae24b48552e0bc81a67713ab4
Rismu854 <rravoavy@gmail.com>
2025-04-08 08:56:17 +0200

    création d'une classe Homme

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,10 +1,14 @@
 package iut.bad;
 
 public class Homme {
-
-	public static void main(String[] args) {
-		System.out.println("Hello !");
-
+	private String nom;
+	private String prenom;
+	private int age;
+	
+	public Homme(String nom, String prenom, int age) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
 	}
 
 }
