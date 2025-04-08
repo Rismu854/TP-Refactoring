ead0c7b090cfc76d5bbc3e5eb38ddca02dc2d0b9
Rismu854 <rravoavy@gmail.com>
2025-04-08 13:44:02 +0200

    Création une classe Humain et modifiez Homme/Femme pour qu'elle soit leur classe parente

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -1,13 +1,7 @@
 package iut.bad;
 
-public class Femme {
-	private String nom;
-	private String prenom;
-	private int age;
-	
-	public Femme(String name,String prenom,int age) {
-		this.nom=nom;
-		this.prenom=prenom;
-		this.age=age;
+public class Femme extends Humain {
+	public Femme(String nom,String prenom,int age) {
+		super(nom ,prenom, age);
 	}
 }
---
+++ src/iut/bad/Humain.java
@@ -0,0 +1,13 @@
+package iut.bad;
+
+public class Humain {
+	protected String nom;
+	protected String prenom;
+	protected int age;
+	
+	public Humain(String nom,String prenom,int age) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+}
