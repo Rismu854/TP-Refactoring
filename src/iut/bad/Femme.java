e1627fdbd376cba7326e010cd448c35a97f08c37
Rismu854 <rravoavy@gmail.com>
2025-04-08 09:03:07 +0200

    Création de la classe Femme

---
+++ src/iut/bad/Femme.java
@@ -0,0 +1,13 @@
+package iut.bad;
+
+public class Femme {
+	private String nom;
+	private String prenom;
+	private int age;
+	
+	public Femme(String name,String prenom,int age) {
+		this.nom=nom;
+		this.prenom=prenom;
+		this.age=age;
+	}
+}
